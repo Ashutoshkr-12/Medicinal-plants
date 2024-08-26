021c39b5a3cb6bedbb4f460adbab5e7811d709e6
Ashutosh <villainhyper@gmail.com>
2024-08-26 17:03:32 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,123 @@
+var a = document.querySelector(".a");
+var b = document.querySelector(".b");
+
+a.addEventListener("mouseover",function(){
+    b.style.opacity= "1"
+ })
+   a.addEventListener("mouseout",function(){
+            b.style.opacity= "0"
+   })
+
+var c = document.querySelector(".c");
+var d = document.querySelector(".d");
+
+c.addEventListener("mouseover",function(){
+    d.style.opacity= "1"
+})
+   c.addEventListener("mouseout",function(){
+            d.style.opacity= "0"
+   })
+
+
+
+var e = document.querySelector(".e");
+var f = document.querySelector(".f");
+
+e.addEventListener("mouseover",function(){
+    f.style.opacity= "1"
+ })
+   e.addEventListener("mouseout",function(){
+            f.style.opacity= "0"
+   });
+  
+
+   var g= document.querySelector(".g");
+   var h = document.querySelector(".h");
+   
+   g.addEventListener("mouseover",function(){
+       h.style.opacity= "1"
+    })
+      g.addEventListener("mouseout",function(){
+               h.style.opacity= "0"
+      });
+     
+      
+   var i= document.querySelector(".i");
+   var j = document.querySelector(".j");
+   
+   i.addEventListener("mouseover",function(){
+       j.style.opacity= "1"
+    })
+      i.addEventListener("mouseout",function(){
+               j.style.opacity= "0"
+      });
+     
+      var k= document.querySelector(".k");
+      var l = document.querySelector(".l");
+      
+      k.addEventListener("mouseover",function(){
+          l.style.opacity= "1"
+       })
+         k.addEventListener("mouseout",function(){
+                  l.style.opacity= "0"
+         });
+        
+         var m= document.querySelector(".m");
+         var n = document.querySelector(".n");
+         
+         m.addEventListener("mouseover",function(){
+             n.style.opacity= "1"
+          })
+            m.addEventListener("mouseout",function(){
+                     n.style.opacity= "0"
+            });
+
+            var o= document.querySelector(".o");
+            var p = document.querySelector(".p");
+            
+            o.addEventListener("mouseover",function(){
+                p.style.opacity= "1"
+             })
+               o.addEventListener("mouseout",function(){
+                        p.style.opacity= "0"
+               });
+
+               var q= document.querySelector(".q");
+               var r = document.querySelector(".r");
+               
+               q.addEventListener("mouseover",function(){
+                  r.style.opacity= "1"
+                })
+                  q.addEventListener("mouseout",function(){
+                           r.style.opacity= "0"
+                  });
+        
+var s = document.querySelector(".s");
+var t= document.querySelector(".t");
+
+ s.addEventListener("mouseover",function(){
+    t.style.opacity= "1"
+ })
+ s.addEventListener("mouseout",function(){
+    t.style.opacity= "0"
+ })
+
+ var u = document.querySelector(".u");
+ var v= document.querySelector(".v")
+
+  u.addEventListener("mouseover",function(){
+    v.style.opacity= "1"
+  })
+  u.addEventListener("mouseout",function(){
+    v.style.opacity= "0"
+  })
+
+  var w=document.querySelector(".w")
+  var x= document.querySelector(".x");
+
+  w.addEventListener("mouseover",function(){
+    x.style.opacity= "1"
+  })
+  w.addEventListener("mouseout",function(){
+    x.style.opacity= "0"
+  })
